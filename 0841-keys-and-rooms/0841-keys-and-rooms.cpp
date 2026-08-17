a93d69d436727f1d70d8194df82f02b48f646659
Paras Agarwal <parasagarwal926@gmail.com>
2026-08-17 12:55:08 +0530

    Time: 0 ms (100%), Space: 14.7 MB (37.56%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -3,29 +3,28 @@ public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         queue<int> keys;
         // visited can be renamed as keyFound for better optics 
-        vector<int> visited;
+        vector<int> visited(rooms.size());
+        visited[0] = 1;
+        int count = 1;
         for(int i = 0; i < rooms[0].size(); i++){
-            if(find(visited.begin(), visited.end(), rooms[0][i]) == visited.end()){
+            if(visited[rooms[0][i]] == 0){
                 keys.push(rooms[0][i]);
-                visited.push_back(rooms[0][i]);
+                visited[rooms[0][i]] = 1;
             }
         }
 
         while(!keys.empty()){
             int i = keys.front();
             keys.pop();
+            count++;
             for(int j = 0; j < rooms[i].size(); j++){
-                if(find(visited.begin(), visited.end(), rooms[i][j]) == visited.end()){
+                if(visited[rooms[i][j]] == 0){
                     keys.push(rooms[i][j]);
-                    visited.push_back(rooms[i][j]);
+                    visited[rooms[i][j]] = 1;
                 }
             }
         }
 
-        if(find(visited.begin(), visited.end(), 0) == visited.end())
-            return visited.size()+1 == rooms.size();
-        else
-            return visited.size() == rooms.size();
-
+        return count == rooms.size();
     }
 };
